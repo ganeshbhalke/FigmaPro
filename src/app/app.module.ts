02551b118a47790aeaedde0a81aa750ddad2bae6
ganeshbhalke <ganeshbhalke2004@gmail.com>
2026-06-08 19:29:55 +0530

    UIResponsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeadersComponent } from './shared/component/headers/headers.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
+import { HeroComponent } from './shared/component/hero/hero.component';
+import { SavicesComponent } from './shared/component/savices/savices.component';
+import { AboutComponent } from './shared/component/about/about.component';
+import { ContactComponent } from './shared/component/contact/contact.component';
+import { HomeComponent } from './shared/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    FooterComponent,
+    HeroComponent,
+    SavicesComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/component/savices/savices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-savices',
+  templateUrl: './savices.component.html',
+  styleUrls: ['./savices.component.scss']
+})
+export class SavicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
